4b75b703a2d3d839252e48fcca588b0795a589cf
Jay Zisch <jay.zisch@wnco.com>
2024-10-22 16:53:53 -0600

    Add Expense Bucket Class with total amount computed variable

--- lib/Model/expense.dart
+++ lib/Model/expense.dart
@@ -34,3 +34,20 @@ enum Category {
   leisure,
   work,
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+    return sum;
+  }
+}
